2972b405890848a5eccd95edd8e8732472e7c229
dolisha <dolishagandhi@gmail.com>
2026-04-01 18:03:41 +0530

    latest

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'/public')));
 app.use(cors({
-  origin:"https://auraahealth.vercel.app",
+  origin:"*",
   methods:["POST","GET","DELETE","PUT"],
   credentials:true,
 }));
