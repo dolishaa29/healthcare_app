197779b5addb9f51608fab535e84d1df7840e6aa
dolisha <dolishagandhi@gmail.com>
2026-04-01 23:49:05 +0530

    latest

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,33 @@
-let express=require("express");
-let path=require("path");
+let express = require("express");
+let path = require("path");
+let dotenv = require("dotenv");
+let cors = require("cors");
+let cookieParser = require("cookie-parser");
 
-let app=express();
-let dotenv=require("dotenv");
 dotenv.config();
 
-app.use(express.json()); 
-app.use(express.urlencoded({extended:true}));
-let cors=require('cors');
+let app = express();
 
-let { health}=require("./dbconnection");
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'/public')));
 app.use(cors({
-  origin:"https://auraahealth.vercel.app",
-  methods:["POST","GET","DELETE","PUT"],
-  credentials:true,
+  origin: "https://auraahealth.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
 }));
-app.use("/",require("./router/adminrouter"));
-app.use("/",require("./router/doctorrouter"));
-app.use("/",require("./router/userrouter"));
-app.use("/",require("./router/appointrouter"));
+
+app.options("*", cors()); // preflight handle karega
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let { health } = require("./dbconnection");
+
+app.use("/", require("./router/adminrouter"));
+app.use("/", require("./router/doctorrouter"));
+app.use("/", require("./router/userrouter"));
+app.use("/", require("./router/appointrouter"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
