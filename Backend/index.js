80c15282950ae4d3c79f6c6fbdd7358a05d40ffe
dolisha <dolishagandhi@gmail.com>
2026-04-07 17:21:20 +0530

    latest

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 let app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://auraahealth.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
