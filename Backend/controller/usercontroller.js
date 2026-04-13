523c3fbf940089121bc75a42ace184187df2bb69
dolisha <dolishagandhi@gmail.com>
2026-04-14 04:22:44 +0530

    new updates

--- Backend/controller/usercontroller.js
+++ Backend/controller/usercontroller.js
@@ -1,4 +1,4 @@
-const { userregister,userlogin,userlogout,userprofile,userlist, userDashboard, userviewapp, otpgenerate, otpverify, blockuser, userbyid} = require("../service/userservice");
+const { userregister,userlogin,userlogout,userprofile,userlist, userDashboard, userviewapp, otpgenerate, otpverify, blockuser, userbyid, changepassword} = require("../service/userservice");
 
 exports.userregister=async(req,res)=>
 {
@@ -48,4 +48,9 @@ exports.blockuser=async(req,res)=>
 exports.userbyid=async(req,res)=>
 {
   await userbyid(req,res);
+}
+
+exports.changepassword=async(req,res)=>
+{
+  await changepassword(req,res);
 }
--- Backend/router/userrouter.js
+++ Backend/router/userrouter.js
@@ -2,7 +2,7 @@ let express=require("express");
 let auth=require("../middleware/user");
 let router=express.Router();
 
-const { userregister, userlogin ,userprofile,userlogout, userlist, userDashboard, userviewapp, userotpgenerate, userotpverify, userbyid } = require("../controller/usercontroller");
+const { userregister, userlogin ,userprofile,userlogout, userlist, userDashboard, userviewapp, userotpgenerate, userotpverify, userbyid, changepassword } = require("../controller/usercontroller");
 const { blockuser } = require("../service/userservice");
 
 router.post("/userregister",userregister);
@@ -16,5 +16,6 @@ router.post("/userforgotpassword",userotpgenerate);
 router.post("/userverifyotp",userotpverify);
 router.post("/blockuser",blockuser);
 router.get("/userbyid/:id",userbyid);
+router.post("/changepassworduser",changepassword);
 
 module.exports=router;
