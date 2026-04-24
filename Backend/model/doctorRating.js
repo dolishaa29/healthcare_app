7030389e5a4da250b7026f946d618d4d83380c51
dolisha <dolishagandhi@gmail.com>
2026-04-25 01:25:01 +0530

    first commit

--- Backend/model/doctorRating.js
+++ Backend/model/doctorRating.js
@@ -1,7 +1,9 @@
 let mongo=require("mongoose");
 let rating=mongo.Schema({
     doctorId:{type:String },
-    userId:{type:String },
+    userId:{type:mongoose.Schema.Types.ObjectId,
+        ref:"doctor"
+    },
     rating:{type:Number },
     description:{type:String},
     date:{type:Date, default:Date.now}
